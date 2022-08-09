bc2112082077d82517a9caf8692319297e953937
huangwantong <huangwantong@escopetech.com>
2022-08-09 11:47:53 +0800

    chore: 3. 无重复字符的最长子串

--- src/topic/3-lengthOfLongestSubstring.ts
+++ src/topic/3-lengthOfLongestSubstring.ts
@@ -2,27 +2,25 @@
  * 3. 无重复字符的最长子串
  * @param {string} s
  * @return {number}
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
  */
 var lengthOfLongestSubstring = function (s) {
-  // 哈希集合，记录每个字符是否出现过
-  const occ = new Set();
-  const n = s.length;
-  // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
-  let rk = -1, ans = 0;
-  for (let i = 0; i < n; ++i) {
-    if (i != 0) {
-      // 左指针向右移动一格，移除一个字符
-      occ.delete(s.charAt(i - 1));
+  let max = 0
+  for (let i = 0; i < s.length; i++) {
+    let map = new Map(), index = 0
+    for (let j = i; j < s.length; j++) {
+      if (map.has(s[j])) {
+        max = Math.max(max, index)
+        break
+      } else {
+        index++
+        map.set(s[j], j)
+      }
     }
-    while (rk + 1 < n && !occ.has(s.charAt(rk + 1))) {
-      // 不断地移动右指针
-      occ.add(s.charAt(rk + 1));
-      ++rk;
-    }
-    // 第 i 到 rk 个字符是一个极长的无重复字符子串
-    ans = Math.max(ans, rk - i + 1);
+
+    max = Math.max(max, index)
   }
-  return ans;
+  return max
 };
 
 
